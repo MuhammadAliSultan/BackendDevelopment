3d4250c66fad2f7dcae9c7caf9694b50deafccc0
MuhammadAliSultan <muhammadalisultan453@gmail.com>
2025-08-03 14:16:43 +0500

    updated routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,4 @@
-import { loginUser, registerUser, logOutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {  registerUser,loginUser,logOutUser,refreshAccessToken,changeCurrentPassword,updateAvatar,updateCoverImage,getCurrentUser,updateUserInfo } from "../controllers/user.controller.js";
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middlerware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
@@ -18,5 +18,10 @@ routes.post("/login", loginUser);
 
 routes.post("/logout", verifyJwt, logOutUser);
 routes.post("/refresh-Token", refreshAccessToken);
+routes.post("/update-avatar", verifyJwt, updateAvatar);
+routes.post("/update-coverImage", verifyJwt, updateCoverImage);
+routes.post("/current-user", verifyJwt, getCurrentUser);
+routes.post("/update-user-info", verifyJwt, updateUserInfo);
+routes.post("/change-current-password", verifyJwt, changeCurrentPassword);
 
 export default routes;
