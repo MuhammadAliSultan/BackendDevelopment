d731d2524ca6b9dba3f6aec341c5b1885d83b7c0
MuhammadAliSultan <muhammadalisultan453@gmail.com>
2025-08-05 15:58:22 +0500

    wrote tweet's controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,10 +1,9 @@
-import mongoose, { isValidObjectId } from "mongoose"
+import mongoose from "mongoose"
 import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
 import { AsyncHandler } from "../utils/AsyncHandler.js";
 import {ApiError} from "../utils/apiError.js"
 import { ApiResponse } from "../utils/apiResponse.js";
-import {uploaderOnCloudinary} from "../utils/cloudinary.js"
+
 
 const createTweet = AsyncHandler(async (req, res) => {
   const { contentOfTweet } = req.body;
@@ -30,14 +29,104 @@ const createTweet = AsyncHandler(async (req, res) => {
 });
 
 const getUserTweets = AsyncHandler(async (req, res) => {
-    // TODO: get user tweets
-})
+  const {
+    page = 1,
+    limit = 10,
+    userId,
+    sortBy = "createdAt",
+    sortType = "desc"
+  } = req.query;
+
+  const matchStage = {};
+  if (userId) {
+    matchStage["owner"] = new mongoose.Types.ObjectId(userId);
+  }
+
+  const sortStage = {
+    [sortBy]: sortType === "asc" ? 1 : -1,
+  };
+
+  const aggregate = Tweet.aggregate([
+    { $match: matchStage },
+    {
+      $lookup: {
+        from: "users", // your User collection name (must be lowercase and plural)
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner"
+      }
+    },
+    { $unwind: "$owner" },
+    { $sort: sortStage },
+    {
+      $project: {
+        content: 1,
+        createdAt: 1,
+        updatedAt: 1,
+        "owner.fullName": 1,
+        "owner.email": 1
+      }
+    }
+  ]);
+
+  const options = {
+    page: parseInt(page),
+    limit: parseInt(limit)
+  };
+
+  const result = await Tweet.aggregatePaginate(aggregate, options);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, "Tweets fetched successfully"));
+});
+
 
 const updateTweet = AsyncHandler(async (req, res) => {
-    //TODO: update tweet
-})
+    const { tweetId } = req.params;
+    const { newContent } = req.body;
+
+    if (!tweetId || !newContent) {
+        throw new ApiError(400, "Tweet ID and new content are required");
+    }
+
+    const tweet = await Tweet.findById(tweetId);
+
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    // Ensure only the tweet owner can update it
+    if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to update this tweet");
+    }
+
+    tweet.content = newContent;
+    await tweet.save();
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, tweet, "Tweet updated successfully"));
+});
+
 
 const deleteTweet = AsyncHandler(async (req, res) => {
+    const { tweetId } = req.params;
+    const { userId } = req.user._id;
+    if(!tweetId||!userId){
+        throw new ApiError(400, "Tweet ID and user ID are required");
+    }
+    const tweet = await Tweet.findById(tweetId);
+    if (!tweet) {
+        throw new ApiError(404, "Tweet not found");
+        }
+     if (tweet.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not authorized to update this tweet");
+    }
+    await tweet.deleteOne();//or remove()
+    return res
+        .status(200)
+        .json(new ApiResponse(200, tweet, "Tweet deleted successfully"));
     //TODO: delete tweet
 })
 
